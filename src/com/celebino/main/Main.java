493ccaa6153966fedce0e86053aa6e1b9b315702
Thayanne <Thayanne@Thayannes-MacBook-Pro.local>
2017-03-25 16:11:45 -0300

    main

--- src/com/celebino/main/Main.java
+++ src/com/celebino/main/Main.java
@@ -1,6 +1,7 @@
 package com.celebino.main;
 
-import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.celebino.persistence.dao.UserDao;

e0df8afbfbe2dc1e8ff68b1269204f5c4616fee5
Thayanne <Thayanne@Thayannes-MacBook-Pro.local>
2017-03-25 13:44:49 -0300

    Implement User Service

--- src/persistence/service/UserService.java
+++ src/persistence/service/UserService.java
@@ -1,5 +1,32 @@
 package persistence.service;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import persistence.dao.UserDAO;
+import persistence.model.User;
+
+@Component
+public class UserService {
+	private UserDAO userDao;
+	
+	public UserDAO getUserDAO(){
+		return userDao;
+	}
+	
+	@Autowired
+	public void setUserDao(UserDAO userDao) {
+	this.userDao = userDao;
+	}
+	 
+	public void addUser(User user) {
+		getUserDAO().insert(user);
+	}
+	 
+	public List<User> fetchAllUsers() {
+	return getUserDAO().selectAll();
+	}
 }
+
